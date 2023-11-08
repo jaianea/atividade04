1a0ed7b88f6267d7b33df86720aeae45818fa14d
jaianea <147431399+jaianea@users.noreply.github.com>
2023-11-08 11:46:08 -0300

    Add files via upload
    atividade04

---
+++ aividade04.py
@@ -0,0 +1,6 @@
+ #Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+numero01 = int(input("numero:"))
+antecessor = (numero01-1)
+print (antecessor)
+sucessor = (numero01+1)
+print (sucessor)
